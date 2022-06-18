5485dceb326016e7a66cb9b92dcb6200078b7e18
zhangyf511 <498104227@qq.com>
2022-06-18 22:31:12 +0800

    2 files modified

--- index.js
+++ index.js
@@ -6,28 +6,31 @@ import { Map } from 'leaflet';
 // appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 //创建高德地图
-const amap = new AMap.Map('amap', {
-  // fadeOnZoom: false,
-  // navigationMode: 'classic',
-  // optimizePanAnimation: false,
-  // animateEnable: false,
-  // dragEnable: false,
-  // zoomEnable: false,
-  // resizeEnble: true,
-  // doubleClickZoom: false,
-  // keyboardEnable: false,
-  // scrollWheel: false,
-  // expandZoomRange: true,
-  // zooms: [1, 20],
-  // mapStyle: 'normal',
-  // features: ['road', 'point', 'bg'],
-  // viewMode: '2D',
+var amap = new Map('amap', {
+  center: [10],
+  fadeOnZoom: false,
+  navigationMode: 'classic',
+  optimizePanAnimation: false,
+  animateEnable: false,
+  dragEnable: false,
+  zoomEnable: false,
+  resizeEnble: true,
+  doubleClickZoom: false,
+  keyboardEnable: false,
+  scrollWheel: false,
+  expandZoomRange: true,
+  zooms: [1, 20],
+  mapStyle: 'normal',
+  features: ['road', 'point', 'bg'],
+  viewMode: '2D',
 });
-
-const map = new Map('map');
+// const amap = new Map('amap');
+var map = new Map('map');
 map.on('zoom', (evt) => {
   amap.setZoom(evt.target.getZoom());
 });
 map.on('move', (evt) => {
-  amap.setZoomAndCenter(evt.target.getZoom(), [pt, lng, pt.lat]);
+  const pt = evt.target.getCenter();
+  amap.setZoomAndCenter(evt.target.getZoom(), [pt.lng, pt.lat]);
 });
+map.setView([39.909186, 116.397411], 10);
